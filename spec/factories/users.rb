834d92418de99679eab0edffc014f3fa06ca6319
Arthur5138 <arthur5138@gmail.com>
2019-02-11 21:32:44 +0900

    add user name to login

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -17,6 +17,7 @@
 #  locked_at              :datetime
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#  name                   :string(255)
 #
 
 FactoryBot.define do
